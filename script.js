a8054ac32df3b2c1ee101463d2ecded43df6ba29
Kennedy Carvalho <kennedy.carvalho@prof.infnet.edu.br>
2026-09-01 22:05:41 -0300

    Aula 7 - #CSS display:block #JS controle de execução (diretivas, escopo)

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+//variável 
+var nome = 'Beltrano';
+let apelido = 'Sicrano';
+const sobrenome = ' Da Silva';
+
+//execução ou chamada da função
+saudacao(nome);
+
+//Função nomeada ou função declarada
+function saudacao(nome) {
+    console.log(nome, apelido, sobrenome);
+    nome = "Fulano;"
+    console.log("Olá, seja bem-vindo(a) " + nome + " !"  );
+}
+
+//console.log(nome);
+
+//expressa
+const oiMundo = function () {
+    console.log("Oi, " + nome + apelido + sobrenome);
+}
+
+oiMundo();
+
 function alternarCartao(idDoCartao) {
     //pega id a partir do ID recebido / passado
     let cartao = document.getElementById(idDoCartao);
 
-
-
     //dentro dele achar o paragrafo extra
     let extra = cartao.querySelector(".oculto");
 
-    console.log(cartao, extra);
+    //console.log(cartao, extra);
+    console.log('extraDisplay:', extra.style.display);
     
     //verificar se está oculto ou não
     if (extra.style.display === "none") {
